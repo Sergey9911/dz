4ad851ba64d529d4339690132accff9c2dc9a610
Sergey9911 <kormilin.sergey@gmail.com>
2024-10-06 22:49:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+the_number_of_completed_t_b=12
+print(the_number_of_completed_t_b)
+number_of_hours_spent=1.5
+print(number_of_hours_spent)
+сourse_name="Python"
+print(сourse_name)
+time_for_one_task=1.5/12
+print(time_for_one_task)
